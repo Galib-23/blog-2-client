3226a7d7bab30dd425c7dd9ad67e8d5b94d334fd
Galib-23 <aagalib2323@gmail.com>
2024-06-14 11:35:27 +0600

    Add theme toggle functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 const Header = () => {
 
     const path = useLocation().pathname;
     const { currentUser } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const { theme } = useSelector(state => state.theme);
 
   return (
     <Navbar className="border-b-2">
@@ -31,10 +34,12 @@ const Header = () => {
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
-      <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color='gray' pill>
-          <FaMoon />
-        </Button>
+      <div className="flex items-center gap-2 md:order-2">
+        <button onClick={() =>dispatch(toggleTheme())} className={`w-9 h-9 rounded-full hidden sm:flex sm:justify-center sm:items-center border hover:shadow-lg hover:scale-105 transition-scale duration-300 ${theme === 'dark' && 'hover:shadow-slate-500 hover:shadow-2xl hover:text-yellow-100'}`}>
+          {
+            theme === 'light' ? <FaSun /> : <FaMoon />
+          }
+        </button>
         {
           currentUser ? (
             <Dropdown arrowIcon={false} inline label={
