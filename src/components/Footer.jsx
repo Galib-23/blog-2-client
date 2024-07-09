3d822078956359823089158addbaa6f2d09e92ea
Galib-23 <aagalib2323@gmail.com>
2024-07-09 22:41:22 +0600

    Add comment section to the post page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
 
 const FooterCom = () => {
   return (
-    <Footer container className="border border-t-8 border-teal-500">
+    <Footer container className="">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
           <div className="flex flex-row sm:flex-col gap-6">
