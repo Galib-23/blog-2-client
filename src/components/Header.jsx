cd834859bf204d64edffe3757de22a35a350dafd
Galib-23 <aagalib2323@gmail.com>
2024-07-14 16:28:47 +0600

    Implement all search features

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,34 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSignOutAlt, FaSun } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
-import { useState } from "react";
-import './Header.css'
+import { useEffect, useState } from "react";
+import "./Header.css";
 import Swal from "sweetalert2";
 import { signOutSuccess } from "../redux/user/userSlice";
 
 const Header = () => {
+  const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { theme } = useSelector((state) => state.theme);
+  const [isSpinning, setIsSpinning] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm)
 
-    const path = useLocation().pathname;
-    const { currentUser } = useSelector((state) => state.user);
-    const dispatch = useDispatch();
-    const { theme } = useSelector(state => state.theme);
-    const [isSpinning, setIsSpinning] = useState(false);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+
+  }, [location.search])
 
   const handleClick = () => {
     setIsSpinning(true);
@@ -31,23 +44,22 @@ const Header = () => {
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
-      confirmButtonText: "Sign Out"
+      confirmButtonText: "Sign Out",
     }).then(async (result) => {
       if (result.isConfirmed) {
-
         try {
-          const res = await fetch('/api/user/signout', {
-            method: 'POST',
+          const res = await fetch("/api/user/signout", {
+            method: "POST",
           });
           const data = await res.json();
           if (!res.ok) {
             console.log(data.message);
-          }else{
+          } else {
             dispatch(signOutSuccess());
             Swal.fire({
               title: "Signed Out",
               text: "You have successfully logged out",
-              icon: "success"
+              icon: "success",
             });
           }
         } catch (error) {
@@ -62,6 +74,14 @@ const Header = () => {
     });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+
   return (
     <Navbar className="border-b-2">
       <Link
@@ -73,72 +93,76 @@ const Header = () => {
         </span>{" "}
         Blog
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
           type="text"
           placeholder="Search..."
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
       <div className="flex items-center gap-2 md:order-2">
-      <button 
-      onClick={handleClick} 
-      className={`w-9 h-9 rounded-full hidden sm:flex sm:justify-center sm:items-center border hover:shadow-lg hover:scale-105 transition-scale duration-300 ${theme === 'dark' && 'hover:shadow-slate-500 hover:shadow-2xl'}`}
-    >
-      {
-        theme === 'light' 
-        ? <FaSun className={`text-yellow-400 ${isSpinning ? 'spin' : ''}`} /> 
-        : <FaMoon className={`${isSpinning ? 'spin' : ''}`} />
-      }
-    </button>
-        {
-          currentUser ? (
-            <Dropdown arrowIcon={false} inline label={
-              <Avatar alt="User" img={currentUser.profilePicture} rounded />
-            }>
-              <Dropdown.Header>
-                <span className="block text-sm font-semibold">{currentUser.username}</span>
-                <span className="block text-sm">{currentUser.email}</span>
-              </Dropdown.Header>
-              <Link to={'/dashboard?tab=profile'}>
-                <Dropdown.Item>Profile</Dropdown.Item>
-              </Link>
-              <Dropdown.Divider />
-              <Dropdown.Item className="text-red-500 flex items-center gap-2" onClick={handleSignout}>
-                Sign Out <FaSignOutAlt />
-              </Dropdown.Item>
-            </Dropdown>
+        <button
+          onClick={handleClick}
+          className={`w-9 h-9 rounded-full hidden sm:flex sm:justify-center sm:items-center border hover:shadow-lg hover:scale-105 transition-scale duration-300 ${
+            theme === "dark" && "hover:shadow-slate-500 hover:shadow-2xl"
+          }`}
+        >
+          {theme === "light" ? (
+            <FaSun className={`text-yellow-400 ${isSpinning ? "spin" : ""}`} />
           ) : (
-            <Link to='/sign-in'>
-            <Button gradientDuoTone='purpleToBlue' outline>
-                Sign In
+            <FaMoon className={`${isSpinning ? "spin" : ""}`} />
+          )}
+        </button>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="User" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm font-semibold">
+                {currentUser.username}
+              </span>
+              <span className="block text-sm">{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item
+              className="text-red-500 flex items-center gap-2"
+              onClick={handleSignout}
+            >
+              Sign Out <FaSignOutAlt />
+            </Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
             </Button>
-        </Link>
-          )
-        }
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-            <Navbar.Link active={path === "/"} as={"div"}>
-                <Link to='/'>
-                    Home
-                </Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === "/about"} as={"div"}>
-                <Link to='/about'>
-                    About
-                </Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === "/projects"} as={"div"}>
-                <Link to='/projects'>
-                    Projects
-                </Link>
-            </Navbar.Link>
-        </Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
